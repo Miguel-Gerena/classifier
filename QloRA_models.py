297a2de8eb01b3af1b6fab58cc32539c0c584f5e
Gmaan2 <miguel.a.gerena@gmail.com>
2024-03-10 21:08:43 -0400

    added all data to loader, mistral is running with higher alpha and stable

--- QloRA_models.py
+++ QloRA_models.py
@@ -17,7 +17,7 @@ from peft import (
 )
 
 
-def get_config(lora_alpha=16, lora_dropout=0.1, lora_r=64):
+def get_config(lora_alpha=128, lora_dropout=0.1, lora_r=64):
     peft_type = PeftType.LORA
     peft_config = LoraConfig(
     r=lora_r,
@@ -26,23 +26,22 @@ def get_config(lora_alpha=16, lora_dropout=0.1, lora_r=64):
     bias="none",
     task_type=TaskType.SEQ_CLS,
     inference_mode=False,
-    target_modules=[
-        "q_proj",
-        "v_proj"
-    ],
+    target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+    
+    
 )
 
     bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
+    # load_in_8bit=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
+    # llm_int8_has_fp16_weight=True,
+    # bnb_4bit_use_double_quant=True,
     bnb_4bit_compute_dtype=torch.bfloat16
 )
     return peft_config, bnb_config
 
-def mistral_QLoRA(args, classes, tokenizer, lora_alpha=16, lora_dropout=0.1, lora_r=64):
+def mistral_QLoRA(args, classes, tokenizer, lora_alpha=128, lora_dropout=0.1, lora_r=64, model_name='mistralai/Mistral-7B-v0.1'):
     peft_config, bnb_config = get_config(lora_alpha, lora_dropout, lora_r)
-    model_name = 'mistralai/Mistral-7B-v0.1'
     model = MistralForSequenceClassification.from_pretrained(
         model_name,
         num_labels=classes,
@@ -52,8 +51,9 @@ def mistral_QLoRA(args, classes, tokenizer, lora_alpha=16, lora_dropout=0.1, lor
     #Setting the Pretraining_tp to 1 ensures we are using the Linear Layers to the max computation possible
 
     #Ensuring the model is aware about the pad token ID
-    model = prepare_model_for_kbit_training(model)
+    model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)
     model = get_peft_model(model, peft_config)
     model.config.pretraining_tp = 1 #For Us this would be 7B
     model.config.pad_token_id = tokenizer.pad_token_id
+
     return model
